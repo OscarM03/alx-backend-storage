1771f77ffe1e690386237c4f5940d6e7e287808d
OscarM03 <oscarmutwiri03@gmail.com>
2024-06-19 17:25:19 +0300

    Storing the inputs and outputs in lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -17,12 +17,30 @@ def count_calls(method: Callable) -> Callable:
     return wrapper
 
 
+def call_history(method: Callable) -> Callable:
+    """Stores history of inputs and outputs"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """actual storage"""
+        input_s = f"{method.__qualname__}:inputs"
+        output_s = f"{method.__qualname__}:outputs"
+
+        self._redis.rpush(input_s, str(args))
+
+        res = method(self, *args, **kwargs)
+        self._redis.rpush(output_s, str(res))
+
+        return res
+    return wrapper
+
+
 class Cache:
     def __init__(self) -> None:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
+    @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Store the input data in Redis using a randomly generated key.
